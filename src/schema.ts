3e9a4f3f4e4452c194098b285dee8f4af0370be0
Jean Damascene CYIFUZO <jeancyifuzodamas@gmail.com>
2022-06-10 10:02:37 +0200

    make it better

--- src/schema.ts
+++ src/schema.ts
@@ -5,8 +5,8 @@ import * as types from "./graphql";   // 1
 export const schema = makeSchema({
     types,   // 2
     outputs: {
-        typegen: join(process.cwd(), "nexus-typegen.ts"),
-        schema: join(process.cwd(), "schema.graphql"),
+        typegen: join(process.cwd(), ".src/nexus-typegen.ts"),
+        schema: join(process.cwd(), ".src/schema.graphql"),
     },
     
     contextType: {  
